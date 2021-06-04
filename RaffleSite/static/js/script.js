421c00bd4118405a5871860d1e8ba312de4ad9f3
Bloodmain <krytoipohtalon@mail.ru>
2021-06-05 03:20:18 +0500

    Redesign

--- RaffleSite/static/js/script.js
+++ RaffleSite/static/js/script.js
@@ -1,24 +1,4 @@
-function OnWindowEvent() {
-    let width = $(window).width() - $('.logo').width() - 4 * $('.first').width()
-    $('.ln').each(function () {
-        $(this).width(width / 4)
-    })
-    var sel = $('.selected')
-    if (sel.offset()) {
-        $('.arrow').offset({left: sel.offset().left + sel.width() / 2.5})
-    }
-}
-
-$(document).ready(function () {
-    OnWindowEvent()
-    $('.header').css('margin-top', 0)
-})
-
-$(window).resize(OnWindowEvent)
-
 $('.nav-elem').click(function () {
     $('.selected').toggleClass('selected')
     $(this).toggleClass('selected')
-    $('.arrow').css('opacity', 1)
-    $('.arrow').offset({left: $(this).offset().left + $(this).width() / 2.5})
 })
