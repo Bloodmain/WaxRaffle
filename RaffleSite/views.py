2ac41181e6c7c9e473ce0f684f0fd6373fd8940b
Bloodmain <krytoipohtalon@mail.ru>
2021-06-16 21:30:48 +0500

    Fixed wrong definitions of pack's types

--- RaffleSite/views.py
+++ RaffleSite/views.py
@@ -7,13 +7,13 @@ import requests
 
 def get_type(img):
     data = {
+        'QmVrg1EWUe15WmAJtUr2cf5Le6TatwH9Ed7qrkD6xVPqfN': 0,  # Test
         'QmWcWnQtu5iAT9TxNYHqtoSooswvbpJgQPUwjzu1ihb7nS': 1,  # Golden Mega
         'QmXUMMThuxypj5fNgp3bnCuWwbUEQsZi5THdETyyKisg8x': 2,  # Golden Mini
         'QmW5w5kczjfwrTNahwxFx63J6bS2y68tEfqEHozVffDsQr': 3,  # Silver Mega
         'QmZHU9toJGB2aZ4Xy7LG3BZ6ThEESyRuViJrUaMw83bfGA': 4,  # Silver Mini
         'QmVWLMawR76ELRC2hqQgdAcC3tFVrAevrLgGUNdnRVWkLP': 5,  # Bronze Mega
-        'Qma8VokEZCNHiRK1HJJUCSviTBxzAyC4avLh67VtGTpn5M': 6,  # Bronze Mini
-        'N/A': 7  # Test
+        'Qma8VokEZCNHiRK1HJJUCSviTBxzAyC4avLh67VtGTpn5M': 6   # Bronze Mini
     }
 
     return data[img]
@@ -39,7 +39,7 @@ def inventory(request):
         'schema_name': '',
         'order': 'transferred_at_time',
         'sort': 'desc',
-        'owner': request.user.username
+        'owner': 'e.hba.wam'  # request.user.username
     }
     req = requests.get('https://wax.api.atomicassets.io/atomicassets/v1/assets', params).json()
     packs = [{'aid': pack['asset_id'],
@@ -47,7 +47,7 @@ def inventory(request):
               'mint': pack['template_mint']
               }
              for pack in req['data']]
-    packs.sort(key=lambda x: x['tp'], reverse=True)
+    packs.sort(key=lambda x: x['tp'])
     return render(request, 'inventory.html',
                   {'title': 'Try Your Luck',
                    'selected': 2,
